dbce7ef144952f7da626e5e09d270937e266de8d
Langeron <dima-dima-9696@mail.ru>
2019-02-23 18:19:54 +0200

    Добавил адаптивное меню

--- source/js/script.js
+++ source/js/script.js
@@ -1,16 +1,7 @@
-var mainNav = document.querySelector('.main-navigation');
-var toggle = document.querySelector('.main-navigation__toggle');
-
-
-// Открытие и закрытие меню
-mainNav.classList.remove('main-navigation--no-js');
-
-toggle.addEventListener('click', function () {
-	if (mainNav.classList.contains('main-navigation--closed')) {
-		mainNav.classList.remove('main-navigation--closed');
-		mainNav.classList.add('main-navigation--opened');
-	} else {
-		mainNav.classList.remove('main-navigation--opened');
-		mainNav.classList.add('main-navigation--closed');
-	}
-});
+$(document).ready( function() {
+	$('.hamburger').click( function() {
+		$('.nav').toggleClass('nav--open');
+		$('body').toggleClass('nav-on');
+		$('html').toggleClass('nav-on');
+	});
+})
