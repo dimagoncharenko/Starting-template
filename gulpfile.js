1d1e46f3cd4eab5c125ed7110658ca1eadf9d3e8
@dgoncharenko <dgoncharenko@crispwp.com>
2019-07-28 12:28:21 +0300

    bug fixed for 4 ver gulp

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,6 @@ var svgstore = require('gulp-svgstore');
 var gulpVersion = 4;
 
 if (gulpVersion === 3) {
-
 	gulp.task('style', function () {
 		gulp.src('source/sass/style.scss')
 			.pipe(plumber())
@@ -161,145 +160,147 @@ if (gulpVersion === 3) {
 	});
 };
 
-
-gulp.task('style', function () {
-	gulp.src('source/sass/style.scss')
-		.pipe(plumber())
-		.pipe(sourcemaps.init())
-		.pipe(sass({
-			includePaths: require('node-normalize-scss').includePaths
-		}))
-		.pipe(postcss([
-			autoprefixer()
-		]))
-		.pipe(gulp.dest('source/css'))
-		.pipe(minify())
-		.pipe(rename('style.min.css'))
-		.pipe(sourcemaps.write())
-		.pipe(gulp.dest('source/css'))
-		.pipe(server.stream());
-});
-
-
-//Сборка стилей в продакшн
-
-gulp.task('styleProd', function () {
-	gulp.src('source/sass/style.scss')
-		.pipe(sass({
-			includePaths: require('node-normalize-scss').includePaths
-		}))
-		.pipe(postcss([
-			autoprefixer()
-		]))
-		.pipe(gulp.dest('build/css'))
-		.pipe(minify())
-		.pipe(rename('style.min.css'))
-		.pipe(gulp.dest('build/css'));
-});
-
-
-// Минифицирую js
-
-gulp.task('js', function () {
-	return gulp.src('source/js/script.js')
-		.pipe(plumber())
-		.pipe(uglify())
-		.pipe(rename({
-			suffix: '.min'
-		}))
-		.pipe(gulp.dest('source/js'))
-		.pipe(server.stream());
-});
-
-
-gulp.task('serve', function () {
-	server.init({
-		server: 'source/',
-		notify: false,
-		open: true,
-		cors: true,
-		ui: false
+if (gulpVersion === 4) {
+	gulp.task('style', function () {
+		return gulp.src('source/sass/style.scss')
+			.pipe(plumber())
+			.pipe(sourcemaps.init())
+			.pipe(sass({
+				includePaths: require('node-normalize-scss').includePaths
+			}))
+			.pipe(postcss([
+				autoprefixer()
+			]))
+			.pipe(gulp.dest('source/css'))
+			.pipe(minify())
+			.pipe(rename('style.min.css'))
+			.pipe(sourcemaps.write())
+			.pipe(gulp.dest('source/css'))
+			.pipe(server.stream());
 	});
 
-	gulp.watch('source/sass/**/*.scss', gulp.parallel('style'));
-	gulp.watch('source/*.html', server.reload);
-	gulp.watch('source/js/**/*.js', gulp.parallel('js'));
-});
 
-gulp.task('watch', gulp.parallel('serve', 'style', 'js'))
+	//Сборка стилей в продакшн
 
-
-gulp.task('copy', function () {
-	return gulp.src([
-		'source/fonts/**/*.{woff,woff2}',
-		'source/img/**',
-		'source/js/**',
-		'source/*.html',
-		'source/libs/**'
-	], {
-			base: 'source'
-		})
-		.pipe(gulp.dest('build'));
-});
-
-
-gulp.task('clean', function () {
-	return del('build');
-});
-
-
-gulp.task('build', function (done) {
-	run(
-		'clean',
-		'js',
-		'copy',
-		'styleProd',
-		done
-	);
-});
-
-
-//Запуск сервера продакшн
-
-gulp.task('serveProd', function () {
-	server.init({
-		server: 'build/',
-		notify: false,
-		open: true,
-		cors: true,
-		ui: false
+	gulp.task('styleProd', function () {
+		return gulp.src('source/sass/style.scss')
+			.pipe(sass({
+				includePaths: require('node-normalize-scss').includePaths
+			}))
+			.pipe(postcss([
+				autoprefixer()
+			]))
+			.pipe(gulp.dest('build/css'))
+			.pipe(minify())
+			.pipe(rename('style.min.css'))
+			.pipe(gulp.dest('build/css'));
 	});
-});
 
 
-gulp.task('images', function () {
-	return gulp.src('source/img/**/*.{png,jpg,svg}')
-		.pipe(imagemin([
-			imagemin.optipng({ optimizationLevel: 3 }),
-			imagemin.jpegtran({ progressive: true }),
-			imagemin.svgo({
-				plugins: [
-					{ removeViewBox: false }
-				]
+	// Минифицирую js
+
+	gulp.task('js', function () {
+		return gulp.src('source/js/script.js')
+			.pipe(plumber())
+			.pipe(uglify())
+			.pipe(rename({
+				suffix: '.min'
+			}))
+			.pipe(gulp.dest('source/js'))
+			.pipe(server.stream());
+	});
+
+
+	gulp.task('serve', function () {
+		server.init({
+			server: 'source/',
+			notify: false,
+			open: true,
+			cors: true,
+			ui: false
+		});
+
+		gulp.watch('source/sass/**/*.scss', gulp.parallel('style'));
+		gulp.watch('source/*.html').on('change', server.reload);
+		gulp.watch('source/js/script.js', gulp.series('js'));
+	});
+
+	gulp.task('watch', gulp.parallel('style', 'js', 'serve'))
+
+
+	gulp.task('copy', function () {
+		return gulp.src([
+			'source/fonts/**/*.{woff,woff2}',
+			'source/img/**',
+			'source/js/**',
+			'source/*.html',
+			'source/libs/**'
+		], {
+				base: 'source'
 			})
-		]))
-		.pipe(gulp.dest('source/img'));
-});
+			.pipe(gulp.dest('build'));
+	});
 
 
-//делаю webp
+	gulp.task('clean', function () {
+		return del('build');
+	});
 
-gulp.task('webp', function () {
-	return gulp.src('source/img/content/**/*.{png,jpg}')
-		.pipe(webp({ quality: 90 }))
-		.pipe(gulp.dest('source/img/content'));
-});
 
-gulp.task('sprite', function () {
-	return gulp.src('source/img/icons/icon-*.svg')
-		.pipe(svgstore({
-			inlineSVG: true
-		}))
-		.pipe(rename('sprite.svg'))
-		.pipe(gulp.dest('source/img/icons'));
-});
+	gulp.task('build', function (done) {
+		run(
+			'clean',
+			'js',
+			'copy',
+			'styleProd',
+			done
+		);
+	});
+
+
+	//Запуск сервера продакшн
+
+	gulp.task('serveProd', function () {
+		server.init({
+			server: 'build/',
+			notify: false,
+			open: true,
+			cors: true,
+			ui: false
+		});
+	});
+
+
+	gulp.task('images', function () {
+		return gulp.src('source/img/**/*.{png,jpg,svg}')
+			.pipe(imagemin([
+				imagemin.optipng({ optimizationLevel: 3 }),
+				imagemin.jpegtran({ progressive: true }),
+				imagemin.svgo({
+					plugins: [
+						{ removeViewBox: false }
+					]
+				})
+			]))
+			.pipe(gulp.dest('source/img'));
+	});
+
+
+	//делаю webp
+
+	gulp.task('webp', function () {
+		return gulp.src('source/img/content/**/*.{png,jpg}')
+			.pipe(webp({ quality: 90 }))
+			.pipe(gulp.dest('source/img/content'));
+	});
+
+	gulp.task('sprite', function () {
+		return gulp.src('source/img/icons/icon-*.svg')
+			.pipe(svgstore({
+				inlineSVG: true
+			}))
+			.pipe(rename('sprite.svg'))
+			.pipe(gulp.dest('source/img/icons'));
+	});
+}
+
